00f21c2ecdd3490fe656d98f6bb4ebce6cc3c301
Francisco Tarantuviez <ft@MacBook-Pro-de-Francisco-2.local>
2024-12-23 21:17:51 -0300

    fix: connect button display

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,10 @@
 import { ToDoList } from "@/components/ToDoList";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex items-center flex-col justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <ConnectButton />
       <ToDoList />
     </div>
   );
